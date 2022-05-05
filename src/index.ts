8f3ae53bd9df6ede26195c51e30302784aeaee30
EvYan1 <104235095+EvYan1@users.noreply.github.com>
2022-05-04 23:20:03 -0300

    cambie nombre de variable sueldo

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-let sueldo12: number = Number(prompt("ingrese su sueldo mensual"));
-let total3: number;
+let sueldo: number = Number(prompt("ingrese su sueldo mensual"));
+let total: number;
 switch (true) {
-  case sueldo12 > 0 && sueldo12 <= 15000:
-    total3 = sueldo12 * 0.2 + sueldo12;
-    console.log("su sueldo actualizado es de ", total3);
+  case sueldo > 0 && sueldo <= 15000:
+    total = sueldo * 0.2 + sueldo;
+    console.log("su sueldo actualizado es de ", total);
     break;
-  case sueldo12 >= 15001 && sueldo12 <= 20000:
-    total3 = sueldo12 * 0.1 + sueldo12;
-    console.log("su sueldo actualizado es de ", total3);
+  case sueldo >= 15001 && sueldo <= 20000:
+    total = sueldo * 0.1 + sueldo;
+    console.log("su sueldo actualizado es de ", total);
     break;
-  case sueldo12 <= 25000:
-    total3 = sueldo12 * 0.05 + sueldo12;
-    console.log("su sueldo actualizado es de ", total3);
+  case sueldo <= 25000:
+    total = sueldo * 0.05 + sueldo;
+    console.log("su sueldo actualizado es de ", total);
     break;
   default:
-    console.log("su sueldo sin aumento es de:", sueldo12);
+    console.log("su sueldo sin aumento es de:", sueldo);
     break;
 }
